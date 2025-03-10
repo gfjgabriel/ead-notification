3b65a3373b1d4726cf682f00031014274e3b25fe
Fortunato <gabriel.fortunato@girotech.com.br>
2025-03-09 21:56:43 -0300

    NotificationModel, service and repository

---
+++ src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ead.notification.services.impl;
+
+import com.ead.notification.repositories.NotificationRepository;
+import com.ead.notification.services.NotificationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository repository;
+}
